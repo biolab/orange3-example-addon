ecef3aa9b26d5ea3d047eefb72ca320184c095ca
Marko Toplak <marko.toplak@gmail.com>
2017-10-27 14:39:07 +0200

    Support widget tests

--- orangecontrib/example/tests/test_example.py
+++ orangecontrib/example/tests/test_example.py
@@ -1,6 +1,17 @@
 import unittest
+from Orange.widgets.tests.base import WidgetTest
+
+from orangecontrib.example.widgets.mywidget import MyWidget
 
 
 class ExampleTests(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+
+
+class TestMyWidget(WidgetTest):
+    def setUp(self):
+        self.widget = self.create_widget(MyWidget)
+
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
